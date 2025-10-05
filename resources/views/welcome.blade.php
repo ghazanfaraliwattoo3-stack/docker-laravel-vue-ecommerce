3ce36da16cd27ac6302fe3e1b54c9d762854972b
ghazanfaraliwattoo <aapka-email@example.com>
2025-10-06 01:49:56 +0500

    Fix mixed content issue: use HTTPS and correct build files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Vue Laravel App</title>
-
-    {{-- Vite CSS + JS ek hi call me --}}
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     
-    {{-- Extra frontend CSS agar chahiye --}}
+    {{-- Laravel Vite CSS --}}
+    @vite('resources/css/app.css')
+
+    {{-- Apka extra CSS --}}
     <link href="{{ asset('frontend/style.css') }}" rel="stylesheet"/>
+
+    {{-- Production build CSS (for render) --}}
+    <link rel="stylesheet" href="/build/assets/app.css">
+
+    <title>Document</title>
 </head>
 <body>
     <div id="app"></div>
+
+    {{-- Laravel Vite JS --}}
+    @vite('resources/js/app.js')
+
+    {{-- Production build JS (for render) --}}
+    <script src="/build/assets/app-X79I1gIu.js"></script>
 </body>
 </html>
