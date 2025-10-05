752e466bb2d85c40a93b2f448d4ef18761534e36
ghazanfaraliwattoo <aapka-email@example.com>
2025-10-06 01:56:18 +0500

    Fix Vite CSS/JS links: remove manual /build/assets links and use @vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     {{-- Laravel Vite CSS --}}
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- Apka extra CSS --}}
     <link href="{{ asset('frontend/style.css') }}" rel="stylesheet"/>
-
-    {{-- Production build CSS (for render) --}}
-    <link rel="stylesheet" href="/build/assets/app.css">
 
     <title>Document</title>
 </head>
 <body>
     <div id="app"></div>
-
-    {{-- Laravel Vite JS --}}
-    @vite('resources/js/app.js')
-
-    {{-- Production build JS (for render) --}}
-    <script src="/build/assets/app-X79I1gIu.js"></script>
 </body>
 </html>
