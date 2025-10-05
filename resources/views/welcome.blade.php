cccf59d37b997ca6f51ce830e9c2faee8ff9de4e
ghazanfaraliwattoo <aapka-email@example.com>
2025-10-06 01:25:08 +0500

    update welcome blade php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    
+    {{-- Update Vite CSS --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
+    {{-- Agar frontend/style.css abhi bhi chahiye to rakh sakte ho --}}
     <link href="{{ asset('frontend/style.css') }}" rel="stylesheet"/>
+    
     <title>Document</title>
 </head>
 <body>
     <div id="app"></div>
-@vite('resources/js/app.js')
 </body>
 </html>
