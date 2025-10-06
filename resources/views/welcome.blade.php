f504d15f7e9f4bdfbba73c7622798f6d99760b98
ghazanfaraliwattoo <aapka-email@example.com>
2025-10-07 00:29:32 +0500

    project final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Vue</title>
     @vite(['resources/js/app.js']) <!-- CSS imported inside app.js -->
+    <link href="{{ asset('frontend/style.css') }}" rel="stylesheet"/>
+
 </head>
 <body>
     <div id="app"></div>
