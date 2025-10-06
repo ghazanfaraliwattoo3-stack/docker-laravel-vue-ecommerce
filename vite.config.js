05cf64eabe85c7cde55cbf9fa7ef1b9d7c15c111
ghazanfaraliwattoo <aapka-email@example.com>
2025-10-06 13:24:12 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/',
-     server: {
-    https: true
-  },
+    base: '/', // relative URLs for production
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: false, // production me auto refresh off
         }),
         vue({
             template: {
@@ -27,6 +24,13 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    build: {
+        manifest: true, // Laravel Vite plugin ke liye
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
         },
     },
 });
